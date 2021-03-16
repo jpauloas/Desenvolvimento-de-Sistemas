831d30f751e87cb26ffbb23a636610863bc1694b
jpauloas <joaopaulosouto@outlook.com>
2021-03-16 02:14:27 -0300

    Atividade 12

---
+++ ATV012/src/Classes/Veiculo.java
@@ -0,0 +1,14 @@
+package Classes;
+public class Veiculo { 
+	private String cor; 
+	private String ano; 
+	private String modelo; 
+	private String placa; 
+	
+	public class Carro extends Veiculo {
+	}
+	
+	public class Moto extends Veiculo {
+	}
+	
+}
