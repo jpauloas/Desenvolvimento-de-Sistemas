118629ca022597c5e1cbdd3f1ebf802c4b7645a3
jpauloas <joaopaulosouto@outlook.com>
2021-04-09 16:27:48 -0300

    06/04

---
+++ Ativ_fix_linkedlist_JoaoPauloAraujoSouto/src/atv/principal.java
@@ -0,0 +1,31 @@
+package atv;
+import java.util.LinkedList;
+
+public class principal {
+	
+	public static void main (String[] args) {
+		LinkedList<String>carros = new LinkedList<String>();
+		
+		// Inserindo valores
+		carros.add("Volkwagem");
+		carros.add("Fiat");
+		carros.add("Ford");
+		carros.addFirst("Ferrari");
+		
+		System.out.println(carros); // Imprimindo carros
+		
+		carros.addFirst("Toyota"); // Inserindo um novo valor na primeira posição
+		
+		carros.addLast("Honda"); // Inserindo um novo valor na ultima posição
+		
+		carros.removeFirst(); // Removendo o primeiro registro da linkedlist
+		
+		carros.removeLast(); // Removendo o ultimo registro da linkedlist
+		
+		System.out.println(carros.getFirst()); //Imprimindo o primeiro valor
+		
+		System.out.println(carros.getLast()); //Imprimindo o ultimo valor
+
+	}
+
+}
